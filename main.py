32502d5118e10dff355d0f4cee2540701f25bbb9
TestPage404 <bers02@yandex.ru>
2023-08-10 17:45:49 +0500

    all pages

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import random
 import arrow
 import os
 import config
+from pprint import pprint
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = config.shared_secret
@@ -118,13 +119,69 @@ def AccountList():
 	clear_data = dictData["SearchResult"]["resources"]
 	return clear_data
 
+def AccountListPage(page):
+	email = '@obghk.ru'
+	url = f"https://10.188.125.122:9060/ers/config/internaluser/?filter=email.CONTAINS.%40obghk.ru&page={page}"
+	payload = {}
+	headers = {
+		'Accept': 'application/json',
+		'Content-Type': 'application/json',
+		'Authorization': 'Basic Z29kb2ZhcGk6V2F1bXo5NHNjSA==',
+		'Cookie': 'APPSESSIONID=39CA9453A74728BE5CB3DA6610829DC1; JSESSIONIDSSO=87978D01CEC651AEEA9AC3F05CAA6B42'
+	}
+	response = requests.request("GET", url, headers=headers, data=payload, verify=False)
+	jsonData = response.text
+	dictData = json.loads(jsonData)
+	clear_data = dictData["SearchResult"]["resources"]
+	return clear_data
+
+def FulldictofAccounts():
+	total = TotalUsers()
+	user_account = {}
+	first_page = AccountList()
+	for string in first_page:
+		user_account[string['name']] = string['description']
+	if total > 20:
+		second_page = AccountListPage(2)
+		for string in second_page:
+			user_account[string['name']] = string['description']
+	if total > 40:
+		third_page = AccountListPage(3)
+		for string in third_page:
+			user_account[string['name']] = string['description']
+	if total > 60:
+		forth_page = AccountListPage(4)
+		for string in forth_page:
+			user_account[string['name']] = string['description']
+	if total > 80:
+		fifth_page = AccountListPage(5)
+		for string in fifth_page:
+			user_account[string['name']] = string['description']
+	return user_account
+
+def TotalUsers():
+	email = '@obghk.ru'
+	url = f"https://{config.ip}:9060/ers/config/internaluser/?filter=email.CONTAINS." + email
+	payload = {}
+	headers = {
+		'Accept': 'application/json',
+		'Content-Type': 'application/json',
+		'Authorization': 'Basic Z29kb2ZhcGk6V2F1bXo5NHNjSA==',
+		'Cookie': 'APPSESSIONID=39CA9453A74728BE5CB3DA6610829DC1; JSESSIONIDSSO=87978D01CEC651AEEA9AC3F05CAA6B42'
+	}
+	response = requests.request("GET", url, headers=headers, data=payload, verify=False)
+	jsonData = response.text
+	dictData = json.loads(jsonData)
+	clear_data = dictData["SearchResult"]["total"]
+	return clear_data
+
 
 @app.route('/', methods=["GET", "POST", "DELETE"])
 def gfg():
 	if 'user' in session:
 		user_list = []
-		for i in AccountList():
-			user_list.append(f"{i['name']} - {i['description']}")
+		for key, value in FulldictofAccounts().items():
+			user_list.append(f"{key} - {value}")
 		flash(AccountCount())
 		for _ in user_list:
 			flash(_)
@@ -163,5 +220,9 @@ def login():
 
 
 if __name__ == '__main__':
-	users.append(User(username='obghk.admin', password=os.environ.get('obghk.admin')))
+	users.append(User(username='obghk.admin', password="12345"))
 	app.run(debug=True, host='0.0.0.0', port='80')
+
+
+
+
